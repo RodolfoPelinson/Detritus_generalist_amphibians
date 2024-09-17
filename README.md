2eabddcf2c6c715798721278ac33bb7b7270c059
Rodolfo Pelinson <rodolfopelinson@gmail.com>
2024-09-17 09:15:47 -0300

    Adding readme file

---
+++ README.md
@@ -0,0 +1,10 @@
+
+<!-- README.md is generated from README.Rmd. Please edit that file -->
+
+# Detritus_generalist_amphibians
+
+<!-- badges: start -->
+<!-- badges: end -->
+
+The goal of Detritus_generalist_amphibians is to make data and code of
+the manuscript “XXXXXX” public.
--- look_before_close.R
+++ look_before_close.R
@@ -1,5 +1,9 @@
-install.packages("renv")
-renv::init()
+
+
+
+
+#install.packages("renv")
+#renv::init()
 
 #Snapshot?
 renv::snapshot()
@@ -8,16 +12,20 @@ renv::restore()
 
 
 #Packages
+install.packages("usethis")
+
 install.packages("devtools")
 library(devtools)
-install.packages("Rcpp")
-devtools::install_github("JenniNiku/gllvm")
+#install.packages("Rcpp")
+#devtools::install_github("JenniNiku/gllvm")
 install.packages("vegan")
 install.packages("glmmTMB")
 install.packages("emmeans")
 install.packages("car")
-install.packages("spaa")
-install.packages("doParallel")
-install.packages("pbapply")
+#install.packages("spaa")
+#install.packages("doParallel")
+#install.packages("pbapply")
 
 
+usethis::use_git()
+usethis::use_github()
